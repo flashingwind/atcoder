fe44b3fc98256836b13301b248d2b2f499854197
munenori <flashingwind@gmail.com>
2023-08-17 22:32:31 +0900

    before 2023

---
+++ abs/src/bin/abc083b.rs
@@ -0,0 +1,14 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+    }
+    let a: [u16; n];
+    for
+    if (a * b) % 2 == 0 {
+        println!("Even");
+    } else {
+        println!("Odd");
+    }
+}
