45d8f56eb32ecd94bc5b7e82635b8a05bddc8603
flashingwind@gmail.com <M. IIDA>
2023-11-09 23:51:53 +0900

    abc248_cとabc255_bが上手くいってない

--- abc255/src/bin/b.rs
+++ abc255/src/bin/b.rs
@@ -7,15 +7,10 @@ fn main() {
         ls_idx: [Usize1;k],
         ps: [(isize,isize);n]
     };
-    let mut obj = ps.clone();
-    for i in ls_idx.iter().rev() {
-        obj.remove(*i);
-    }
-    println!("{:?}", obj);
     let mut max = 0;
     for &i in ls_idx.iter() {
-        for &(x, y) in obj.iter() {
-            let d = ps[i].0.abs_diff(x).pow(2) + ps[i].1.abs_diff(y).pow(2);
+        for &(x, y) in ps.iter() {
+            let d = (x - ps[i].0).pow(2) + (y - ps[i].1).pow(2);
             max = max.max(d);
         }
     }
