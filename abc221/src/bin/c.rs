96543c33e8c1ae1fe6272916df3f3e7597d10351
flashingwind@gmail.com <M. IIDA>
2023-11-11 18:12:48 +0900

    ABC221-C

--- abc221/src/bin/c.rs
+++ abc221/src/bin/c.rs
@@ -7,32 +7,45 @@ fn main() {
     input! {
         n: Chars,
     };
-    let i_set_n: HashSet<usize> = HashSet::from_iter(0..n.len());
+    let i_set_all: HashSet<usize> = HashSet::from_iter(0..n.len());
     let mut max = 0;
     for len in 1..=n.len() - 1 {
         for pat in (0..n.len()).combinations(len) {
             let i_set_a: HashSet<_> = pat.iter().map(|v| *v).collect();
-            let i_set_b = &i_set_n - &i_set_a;
-            if i_set_a.len() == 0 || i_set_b.len() == 0 {
-                continue;
-            }
-            println!("set a={:?} set b={:?}", i_set_a, i_set_b);
+            let i_set_b = &i_set_all - &i_set_a;
+            // if i_set_a.len() == 0 || i_set_b.len() == 0 {
+            //     continue;
+            // }
+
             let a: u64 = i_set_a
-                .iter()
-                .map(|v| n[*v].to_string())
-                .sorted()
-                .rev()
-                .fold("".to_string(), |str, c| str + c.as_str())
-                .parse()
-                .unwrap();
+            .iter()
+            .map(|i|n[*i])
+            .sorted()
+            .rev()
+            .fold("".to_string(), |str, c| str+c.to_string().as_str())
+            .parse()
+            .unwrap();
             let b: u64 = i_set_b
-                .iter()
-                .map(|v| n[*v].to_string())
-                .sorted()
-                .rev()
-                .fold("".to_string(), |str, c| str + c.as_str())
-                .parse()
-                .unwrap();
+            .iter()
+            .map(|i|n[*i])
+            .sorted()
+            .rev()
+            .fold("".to_string(), |str, c| str+c.to_string().as_str())
+            .parse()
+            .unwrap();
+        // println!("set a={:?} set b={:?}", i_set_a
+        //     .iter()
+        //     .map(|i|n[*i])
+        //     .sorted()
+        //     .rev()
+        //     .fold("".to_string(), |str, c| str+c.to_string().as_str()),
+        // i_set_b
+        //     .iter()
+        //     .map(|i|n[*i])
+        //     .sorted()
+        //     .rev()
+        //     .fold("".to_string(), |str, c| str+c.to_string().as_str()));
+        // println!("{a}*{b}={}",a * b);
             max = max.max(a * b);
         }
     }
