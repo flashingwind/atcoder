79563adfb777599269d0c36fbff921f59beb7949
flashingwind@gmail.com <M. IIDA>
2024-04-29 20:13:42 +0900

    ARC131: B修正

--- arc131/src/bin/b.rs
+++ arc131/src/bin/b.rs
@@ -5,42 +5,33 @@ fn main() {
     input! {
         h: usize,
         w: usize,
-        c: [Chars;h],
+        mut c: [Chars;h],
     };
-    let ds = vec![
-        (-1_isize, -1_isize),
-        (0, -1),
-        (1, -1),
-        (-1, 0),
-        (1, 0),
-        (-1, 1),
-        (-1, 1),
-        (0, 1),
-        (1, 1),
-    ];
-
-    let mut is_changed;
-    let mut all_colors = HashSet::new();
-    let mut possible=vec![vec![HashSet::new();w];h];
-    let
-    ['1', '2', '3', '4', '5'].iter().map(|v| all_colors.insert(*v));
-    loop {
-        is_changed = false;
-
-        for i in 0..h {
-            for j in 0..w {
-                let mut set = HashSet::from(
-                    ds.iter()
-                        .filter(|&&(di, dj)| (di.abs() as usize) < i && (dj.abs() as usize) < j)
-                        .map(|&(di, dj)| c[(i as isize + di) as usize][(j as isize + dj) as usize])
-                        .collect::<HashSet<char>>(),
-                );
-                possible[i][j] = &all_colors - &set;
-                if diff.len()
+    let ds = vec![(-1_isize, -1_isize), (0, -1), (-1, 0), (1, 0), (0, 1)];
+    let allcol = HashSet::from([1, 2, 3, 4, 5]);
+    for i in 0..h {
+        for j in 0..w {
+            let mut colors = HashSet::new();
+            for &d in ds.iter() {
+                let (ii, jj) = (i as isize + d.0, j as isize + d.1);
+                if 0 <= ii && ii < h as isize && 0 <= jj && jj < w as isize {
+                    if c[ii as usize][jj as usize] == '.' {
+                    } else {
+                        colors.insert(c[ii as usize][jj as usize].to_digit(10).unwrap());
+                    }
+                }
+            }
+            let diffcol = &allcol ^ &colors;
+            for d in &diffcol {
+                c[i][j] = d.to_string().chars().nth(0).unwrap();
+                break;
             }
         }
-        if !is_changed {
-            break;
+    }
+    for cc in c.iter() {
+        for ccc in cc.iter() {
+            print!("{}", *ccc);
         }
+        println!();
     }
 }
